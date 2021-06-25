83d951954c7cb40731565adf9cb3bd8ef2ae0e37
paniniswap <harambe.satoshi@gmail.com>
2021-06-25 23:19:01 +0800

    enabled sending transaction again

--- src/dsa-connect/src/dsa.ts
+++ src/dsa-connect/src/dsa.ts
@@ -365,7 +365,6 @@ export class DSA {
     if (!mergedParams.to) throw new Error(`Parameter 'to' is not defined.`)
 
     const data = await this.getData(mergedParams)
-    console.log('data', data)
 
     const transactionConfig = await this.internal.getTransactionConfig({
       from: mergedParams.from,
@@ -380,10 +379,9 @@ export class DSA {
     console.log(`DSA config:\n version: ${this.instance.version}\n chainId: ${this.instance.chainId}`)
     console.log(`Casting spells to DSA(#${this.instance.id})...`)
 
-    // const transaction = await this.transaction.send(transactionConfig)
+    const transaction = await this.transaction.send(transactionConfig)
 
-    // return transaction
-    return ''
+    return transaction
   }
 
   private async getData(params: { spells: Spells; origin?: string }) {
